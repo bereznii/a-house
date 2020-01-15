351794f90b4c084975b0eb2aa1e7a74674901d6e
bereznii <dima.berezniy.13@gmail.com>
2020-01-15 16:16:46 +0200

    Sidebar modified

--- resources/views/client/sidebar.blade.php
+++ resources/views/client/sidebar.blade.php
@@ -1,10 +1,60 @@
 <div class="col-lg-3">
 
-    <h1 class="my-4">Shop Name</h1>
-    <div class="list-group">
-        <a href="#" class="list-group-item">Category 1</a>
-        <a href="#" class="list-group-item">Category 2</a>
-        <a href="#" class="list-group-item">Category 3</a>
+    <h1 class="my-4">"LOGO"</h1>
+
+    <div class="form-group">
+        <div class="input-group">
+            <input type="text" placeholder="..." class="form-control" aria-label="Поиск">
+            <div class="input-group-append">
+                <button type="button" class="btn btn-outline-secondary">Поиск</button>
+                <button type="button" class="btn btn-outline-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span class="sr-only">Toggle Dropdown</span>
+                </button>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#">Поиск по еврокоду</a>
+                    <a class="dropdown-item" href="#">Поиск по сканкоду</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <hr>
+
+    <div class="form-group">
+        <label for="exampleFormControlSelect1">Модель:</label>
+        <select class="form-control" id="exampleFormControlSelect1">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+        </select>
+    </div>
+
+    <div class="form-group">
+    <label for="exampleFormControlSelect2">Марка:</label>
+        <select class="form-control" id="exampleFormControlSelect2">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+        </select>
+    </div>
+
+    <div class="form-group">
+        <label for="exampleFormControlSelect3">Тип стекла:</label>
+        <select class="form-control" id="exampleFormControlSelect3">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+        </select>
+    </div>
+
+    <div class="form-group">
+        <button class="btn btn-primary btn-lg btn-block">Подобрать</button>
     </div>
 
 </div>
