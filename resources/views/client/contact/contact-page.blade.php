285b67d79bde58c27df60d53c883fdad99fe67ac
bereznii <dima.berezniy.13@gmail.com>
2020-02-05 11:07:05 +0200

    do not show on large screens mobile cart

--- resources/views/client/contact/contact-page.blade.php
+++ resources/views/client/contact/contact-page.blade.php
@@ -2,10 +2,11 @@
 
     <div class="card my-4">
         <div class="card-header">
-            Контакты
+            Контактные данные
         </div>
         <div class="card-body">
-            Content
+            <p>Обращайтесь по любым вопросам! Поможем Вам подобрать любое стекло на любой автомобиль.</p>
+            <p>Электронная почта: <b>autoglasshouse20@gmail.com</b></p>
         </div>
     </div>
 
