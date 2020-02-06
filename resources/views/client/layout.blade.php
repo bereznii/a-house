cbe947160cc327b4d35d37dca7c773224bdfe118
bereznii <dima.berezniy.13@gmail.com>
2020-02-06 19:07:49 +0200

    no index added

--- resources/views/client/layout.blade.php
+++ resources/views/client/layout.blade.php
@@ -20,7 +20,7 @@
     <title>{{ $metaData['title'] ?? 'Интернет-магазин Autoglass House' }}</title>
     <meta name="description" content="{{ $metaData['description'] ?? 'Интернет-магазин Autoglass House' }}">
     <meta name="keywords" content="{{ $metaData['keywords'] ?? '' }}">
-    <meta name="robots" content="index, follow" />
+    <meta name="robots" content="noindex, nofollow" />
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
