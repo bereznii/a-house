2017cd4e3370f3a45035960cf833ac8be1cfd7ca
bereznii <dima.berezniy.13@gmail.com>
2020-02-10 17:36:49 +0200

    sitemap modified

--- resources/views/client/sitemap/index.blade.php
+++ resources/views/client/sitemap/index.blade.php
@@ -1,13 +1,13 @@
 <?php '<?xml version="1.0" encoding="UTF-8"?>' ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-{{--    @foreach ($products as $product)--}}
-{{--        <url>--}}
-{{--            <loc>{{url("automotive/{$product->id}")}}</loc>--}}
-{{--            <lastmod>{{ $product->updated_at->tz('GMT')->toAtomString() }}</lastmod>--}}
-{{--            <changefreq>always</changefreq>--}}
-{{--            <priority>1.0</priority>--}}
-{{--        </url>--}}
-{{--    @endforeach--}}
+    @foreach ($products as $product)
+        <url>
+            <loc>{{url("automotive/{$product->id}")}}</loc>
+            <lastmod>{{ $product->updated_at->tz('GMT')->toAtomString() }}</lastmod>
+            <changefreq>always</changefreq>
+            <priority>1.0</priority>
+        </url>
+    @endforeach
         <url>
             <loc>https://autoglasshouse.com.ua/checkout</loc>
             <lastmod>{{ now()->tz('GMT')->toAtomString() }}</lastmod>
