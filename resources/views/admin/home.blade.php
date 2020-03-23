2d6d0d8b8d8e824e62ac16d2bcf20577d0cf00bc
bereznii <dima.berezniy.13@gmail.com>
2020-03-23 22:31:56 +0200

    fix

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -8,7 +8,7 @@
             <a href="{{ route('home.manufacturer-charge') }}" class="list-group-item @if(route('home.manufacturer-charge') == url()->current()) active @endif">Наценка по поставщикам</a>
             <a href="{{ route('home.callbacks') }}" class="list-group-item @if(route('home.callbacks') == url()->current()) active @endif">Запросы на обратный звонок</a>
             <a href="{{ route('orders.index') }}" class="list-group-item @if(route('orders.index') == url()->current()) active @endif">Заказы</a>
-            <a href="{{ route('models.index') }}" class="list-group-item @if(route('models.index') == url()->current()) active @endif">Дополнительная информация по моделяи</a>
+            <a href="{{ route('models.index') }}" class="list-group-item @if(route('models.index') == url()->current()) active @endif">Дополнительная информация по моделям</a>
         </div>
 
     </div>
