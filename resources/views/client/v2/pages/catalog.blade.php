98aba851ef5d79bb1c4def24744f2ebfa5b51d88
bereznii <dima.berezniy.13@gmail.com>
2020-07-16 18:43:30 +0300

    feat: add default schema.org

--- resources/views/client/v2/pages/catalog.blade.php
+++ resources/views/client/v2/pages/catalog.blade.php
@@ -23,6 +23,24 @@
             ]
         }
         </script>
+    @else
+        <script type="application/ld+json">
+         {
+         "@context": "https://schema.org",
+         "@type": "Organization",
+         "name": " Autoglass House",
+         "legalName": "Autoglass House",
+         "url": "https://autoglasshouse.com.ua",
+         "description": "Наш интернет-магазин 'Autoglass House' осуществляет продажу и установку автомобильных стёкол таких мировых брендов как (Sekurit, Pilkington, AGC, Splintex, Autover, PGW и другие), а также Украинского производителя SafeGlass. Мы подберём Вам любое стекло на любой автомобиль. Если вам неудобно приехать к нам, мы доставим и установим(при соответствующих условиях) стекло по указаному адресу. Для Вашего удобства у нас есть выездной сервис.Нашим клиентам с других регионов, доставим стекло перевозчиком 'Новая почта', 'Гюнсел' и др. Всегда готовы помочь Вам в вопросе подбора, доставки и установки нужного стекла!",
+         "logo": "{{ asset('storage/logo.png') }}",
+         "foundingDate": "2020",
+         "contactPoint": {
+             "@type": "ContactPoint",
+             "contactType": "Customer support",
+             "email": "autoglasshouse20@gmail.com"
+            }
+         }
+        </script>
     @endif
 
     @include('client.v2.sections.breadcrumbs')
