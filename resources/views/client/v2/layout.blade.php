7b80bff318e1529235538d28ae58dc61ae328e58
bereznii <dima.berezniy.13@gmail.com>
2020-07-18 15:53:56 +0300

    feat: add calendar to top-bar

--- resources/views/client/v2/layout.blade.php
+++ resources/views/client/v2/layout.blade.php
@@ -71,6 +71,7 @@
             <div class="contact-info mr-auto">
                 <i class="icofont-envelope"></i> <a href="mailto:autoglasshouse20@gmail.com">autoglasshouse20@gmail.com</a>
                 <i class="icofont-phone"></i> <a href="tel:+380986921349">+38 (098) 692 13 49</a>
+                <i class="icofont-calendar"></i> <a href="#"> Пн-Пт.: с 8:00 до 18:00</a>
             </div>
         </div>
     </div>
