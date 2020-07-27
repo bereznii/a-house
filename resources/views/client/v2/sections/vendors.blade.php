465744274e995279086b147d93e674506401404f
bereznii <dima.berezniy.13@gmail.com>
2020-07-27 20:39:44 +0300

    fix: paths to vendors imgs

--- resources/views/client/v2/sections/vendors.blade.php
+++ resources/views/client/v2/sections/vendors.blade.php
@@ -11,14 +11,14 @@
         </div>
 
         <div class="owl-carousel clients-carousel">
-            <img src="{{ asset('storage/img/pilkington-trans.png') }}" alt="Pilkington Logo">
-            <img src="{{ asset('storage/img/sekurit-trans.png') }}" alt="Sekurit Logo">
-            <img src="{{ asset('storage/img/agc-trans.png') }}" alt="AGC Logo">
-            <img src="{{ asset('storage/img/sg-trans.png') }}" alt="Safeglass Logo">
-            <img src="{{ asset('storage/img/pgw-trans.png') }}" alt="PGW Logo">
-            <img src="{{ asset('storage/img/starglass-trans.png') }}" alt="Starglass Logo">
-            <img src="{{ asset('storage/img/nordglass-trans.png') }}" alt="Nordglass Logo">
-            <img src="{{ asset('storage/img/guardianglass-trans.png') }}" alt="Guardianglass Logo">
+            <img src="{{ asset('landing_img/vendors/pilkington-trans.png') }}" alt="Pilkington Logo">
+            <img src="{{ asset('landing_img/vendors/sekurit-trans.png') }}" alt="Sekurit Logo">
+            <img src="{{ asset('landing_img/vendors/agc-trans.png') }}" alt="AGC Logo">
+            <img src="{{ asset('landing_img/vendors/sg-trans.png') }}" alt="Safeglass Logo">
+            <img src="{{ asset('landing_img/vendors/pgw-trans.png') }}" alt="PGW Logo">
+            <img src="{{ asset('landing_img/vendors/starglass-trans.png') }}" alt="Starglass Logo">
+            <img src="{{ asset('landing_img/vendors/nordglass-trans.png') }}" alt="Nordglass Logo">
+            <img src="{{ asset('landing_img/vendors/guardianglass-trans.png') }}" alt="Guardianglass Logo">
         </div>
 
     </div>
