28eee963157280547ad60bd9312a6cd3af14ddef
Dmytro <dima.berezniy.13@gmail.com>
2020-07-31 14:13:36 +0300

    fix: preload css

--- resources/views/client/v2/layout.blade.php
+++ resources/views/client/v2/layout.blade.php
@@ -30,16 +30,16 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!-- Google Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+        <link rel="preload" as="style" href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" onload="this.rel='stylesheet'">
 
         <!-- Vendor CSS Files -->
-        <link href="/landing_assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="/landing_assets/icofont/icofont.min.css" rel="stylesheet">
-        <link href="/landing_assets/animate.css/animate.min.css" rel="stylesheet">
-        <link href="/landing_assets/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+        <link rel="preload" as="style" href="/landing_assets/bootstrap/css/bootstrap.min.css" onload="this.rel='stylesheet'">
+        <link rel="preload" as="style" href="/landing_assets/icofont/icofont.min.css" onload="this.rel='stylesheet'">
+        <link rel="preload" as="style" href="/landing_assets/animate.css/animate.min.css" onload="this.rel='stylesheet'">
+        <link rel="preload" as="style" href="/landing_assets/owl.carousel/assets/owl.carousel.min.css" onload="this.rel='stylesheet'">
 
         <!-- Template Main CSS File -->
-        <link href="/css/style.css" rel="stylesheet">
+        <link rel="preload" as="style" href="/css/style.css" onload="this.rel='stylesheet'">
 
         @if(route('new-client.landing') !== $pageData['currentUrl'])
             <style>
@@ -77,18 +77,18 @@
         <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
 
         <!-- Vendor JS Files -->
-        <script src="/js/jquery-3.5.1.min.js"></script>
-        <script src="/landing_assets/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <script src="/landing_assets/jquery.easing/jquery.easing.min.js"></script>
-        <script src="/landing_assets/owl.carousel/owl.carousel.min.js"></script>
-        <script src="/landing_assets/isotope-layout/isotope.pkgd.min.js"></script>
+        <script defer src="/js/jquery-3.5.1.min.js"></script>
+        <script defer src="/landing_assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script defer src="/landing_assets/jquery.easing/jquery.easing.min.js"></script>
+        <script defer src="/landing_assets/owl.carousel/owl.carousel.min.js"></script>
+        <script defer src="/landing_assets/isotope-layout/isotope.pkgd.min.js"></script>
 
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+        <script defer src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
         <!-- Template Main JS File -->
-        <script src="/js/main.js"></script>
-        <script src="/js/shop-scripts.js"></script>
+        <script defer src="/js/main.js"></script>
+        <script defer src="/js/shop-scripts.js"></script>
 
     </body>
 </html>
