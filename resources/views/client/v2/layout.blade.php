b956bfac00bc6cd0fbee4e39f27dd3eaaa68b9c3
bereznii <dima.berezniy.13@gmail.com>
2020-10-26 19:20:19 +0200

    feat: new ga tag

--- resources/views/client/v2/layout.blade.php
+++ resources/views/client/v2/layout.blade.php
@@ -3,12 +3,14 @@
     <head>
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-157621129-1"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-181175887-1"></script>
         <script>
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
             gtag('config', 'UA-157621129-1');
+            gtag('config', 'UA-181175887-1');
         </script>
 
         <meta charset="utf-8">
