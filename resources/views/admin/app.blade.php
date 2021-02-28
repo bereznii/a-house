8eed70b865be96fa48d9c56bbba1ab60a03b8484
dmytro <dima.berezniy.13@gmail.com>
2021-02-28 19:35:50 +0200

    feat: namings

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Autoglass House') }} Admin Panel</title>
+    <title>Autoglass House Админка</title>
     <link rel="icon" href="{{ asset('storage/icon-sm.ico') }}" type="image/icon type">
 
     <!-- Scripts -->
@@ -28,7 +28,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    Autoglass House Админка
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -51,7 +51,7 @@
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
+                                    Выйти
                                 </a>
 
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
